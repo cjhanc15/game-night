8198de19f00bac823c39bace9a0328984963f24c
Cybyl Jade <cybyl.hancock@us.af.mil>
2022-09-21 13:50:21 -0600

    basic framework for application

--- ui/src/config.js
+++ ui/src/config.js
@@ -5,7 +5,7 @@ const config = {
     },
 
     production: {
-        apiUrl: "https://bsdi-poc-api.herokuapp.com"
+        apiUrl: "https://game-nights-api.herokuapp.com"
     },
     test: {
         apiUrl: ''
